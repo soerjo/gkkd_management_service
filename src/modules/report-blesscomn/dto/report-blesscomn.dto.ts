cd10f073ae31a4064c9f6d38e0c3a0922fece7a8
soerjo <ryohastomo@gmail.com>
2024-01-02 14:21:16 +0700

    update: module blesscomn & module region - update dto create

--- src/modules/report-blesscomn/dto/report-blesscomn.dto.ts
+++ src/modules/report-blesscomn/dto/report-blesscomn.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { Transform } from "class-transformer"
 import { IsDateString, IsNotEmpty, IsNumber, Min } from "class-validator"
 
 export class ReportBlesscomnDto {
@@ -10,24 +11,28 @@ export class ReportBlesscomnDto {
     @ApiProperty()
     @IsNumber()
     @Min(0)
+    @Transform(val => Number(val.value))
     @IsNotEmpty()
     total_male: number
 
     @ApiProperty()
     @IsNumber()
     @Min(0)
+    @Transform(val => Number(val.value))
     @IsNotEmpty()
     total_female: number
 
     @ApiProperty()
     @IsNumber()
     @Min(0)
+    @Transform(val => Number(val.value))
     @IsNotEmpty()
     total: number
 
     @ApiProperty()
     @IsNumber()
     @Min(0)
+    @Transform(val => Number(val.value))
     @IsNotEmpty()
     new: number
 }
