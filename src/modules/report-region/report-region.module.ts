b7eecd31ef78a7f9ff2105dc0b2f15351df5a83b
soerjo <ryohastomo@gmail.com>
2024-01-02 15:51:23 +0700

    update: module report region - import export module region

--- src/modules/report-region/report-region.module.ts
+++ src/modules/report-region/report-region.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportRegionService } from './services/report-region.service';
 import { ReportRegionController } from './controller/report-region.controller';
 import { ReportRegionRepository } from './repository/report-region.repository';
+import { RegionModule } from '../region/region.module';
 
 @Module({
+  imports: [RegionModule],
   controllers: [ReportRegionController],
   providers: [ReportRegionService, ReportRegionRepository],
 })
