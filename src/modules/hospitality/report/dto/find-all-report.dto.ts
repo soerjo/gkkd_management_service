8d8b33fe2d842245832649c1d8e96c96d47549fc
soerjo <ryohastomo@gmail.com>
2025-05-14 18:47:27 +0700

    refactor: takeout dto query params to get sunday service report

--- src/modules/hospitality/report/dto/find-all-report.dto.ts
+++ src/modules/hospitality/report/dto/find-all-report.dto.ts
@@ -3,7 +3,7 @@ import { Transform, Type } from "class-transformer";
 import { IsBoolean, IsDate, IsNumber, IsOptional } from "class-validator";
 import { PaginationDto } from "src/common/dto/pagination.dto";
 
-export class FindAllReportDto extends PaginationDto { 
+export class FindAllReportDto { 
     @ApiProperty()
     @Type(() => Date)
     @IsDate()
@@ -13,13 +13,6 @@ export class FindAllReportDto extends PaginationDto {
     @Type(() => Number)
     @IsNumber()
     sunday_service_id: number;
-
-    @ApiPropertyOptional({ type: Boolean })
-    @IsOptional()
-    // @Type(() => Boolean)
-    @Transform(({ value }) => value === 'true')
-    @IsBoolean()
-    isVersion: boolean;
 
     region_id?: number;
 }
