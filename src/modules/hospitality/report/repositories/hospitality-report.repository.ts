249dabaa47c4d4691abdedd4681396386813bb52
soerjo <ryohastomo@gmail.com>
2025-05-17 10:04:47 +0700

    refactor: filter by alias in report

--- src/modules/hospitality/report/repositories/hospitality-report.repository.ts
+++ src/modules/hospitality/report/repositories/hospitality-report.repository.ts
@@ -65,7 +65,7 @@ export class HospitalityReportRepository extends Repository<HospitalityReportEnt
     
     queryBuilder.andWhere('hospitality_data.region_id = :region_id', { region_id: filter.region_id });
     filter.isVersion && queryBuilder.andWhere('hospitality_report.id is not null');
-    filter.name && queryBuilder.andWhere('hospitality_data.name ILIKE :search', { search: `%${filter.name}%` });
+    filter.name && queryBuilder.andWhere('hospitality_data.name ILIKE :search OR hospitality_data.alias ILIKE :search', { search: `%${filter.name}%` });
     filter.region_id && queryBuilder.andWhere('hospitality_data.region_id = :region_id', { region_id: filter.region_id });
     filter.segment_id && queryBuilder.andWhere('hospitality_data.segment_id = :segment_id', { segment_id: filter.segment_id });
     filter.blesscomn_id && queryBuilder.andWhere('hospitality_data.blesscomn_id = :blesscomn_id', { blesscomn_id: filter.blesscomn_id });
